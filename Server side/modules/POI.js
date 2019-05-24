75fc19cd6a6d3d2fa2c427276f1254da1ec2d18a
galvaitzman <galvaitzman@gmail.com>
2019-05-24 15:35:24 +0300

    add Goni version

--- Server side/modules/POI.js
+++ Server side/modules/POI.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var app = express();
 var DButilsAzure = require('../DButils');
-//var Regex = require("regex");
+var Regex = require("regex");
 var jwt = require('jsonwebtoken');
 var  superSecret = "SUMsumOpen";
-var currentUserName = "galvai";
+var currentUserName = 'galvai';
 
 
 router.get('/getThreePopularRandomPoints',function (req,res) 
 {
 var threshHole = 4;
-DButilsAzure.execQuery("select * from POIS where RANK >= "+threshHole)
+DButilsAzure.execQuery("select * from POIS where RANK >= "+threshHole+"")
 .then(function(ans) {
     if (ans.length>3){
         ans = shuffle(ans);
@@ -21,36 +21,38 @@ DButilsAzure.execQuery("select * from POIS where RANK >= "+threshHole)
     var finalAns = [];
     while (i<Math.min(3,ans.length)){
         finalAns.push(ans[i]);
-		i=i+1;
+        i=i+1;
     }  
     res.send(finalAns);                    
-	}
-	
+    }
 )
 .catch(ans=>res.send("" +ans));
 });
 
 router.get('/getTwoMostPopularPointsBasedOnUserCategories',function (req,res) {
-	var sql ="select POI_ID,CATEGORY from POIS where Category in( select CATEGORY from FAVOURITE_CATEGORIES where USER_NAME ='"+currentUserName+"') order by RANK DESC";
+	var sql ="select NAME,CATEGORY from POIS where Category in( select CATEGORY from FAVOURITE_CATEGORIES where USER_NAME ='"+currentUserName+"') order by RANK DESC";
 	DButilsAzure.execQuery(sql)    
 	.then(function(ans) {
-		var POI1 = ans[0].POI_ID;
+        if (ans.length == 0 ){
+            return Promise.reject('no Point of Interest from the given category');
+        }
+		var POI1 = ans[0].NAME;
 		var category1 = ans[0].CATEGORY;		
 		var POI2;
 		var i = 1;
-		var foundPOIfromAnotherCategory=false	;		
+		var foundPOIfromAnotherCategory=false			
 		while (i<ans.length && foundPOIfromAnotherCategory==false) {			
-			if (ans[i].CATEGORY !== category1){					
-				POI2 = ans[i].NAME;
+			if (ans[i].CATEGORY_NAME != category1){					
+				POI2 = ans[i].Name;
 				foundPOIfromAnotherCategory=true							
 			}		
 			i++;		
 		}
 		var query;
-		if(foundPOIfromAnotherCategory == false)
-			 query="SELECT * FROM POIS WHERE POI_ID in ('"+POI1+"')";
+		if(!foundPOIfromAnotherCategory)
+			 query="SELECT * FROM POIS WHERE Name in ('"+POI1+"')";
 		else
-			 query = "SELECT * FROM POIS WHERE POI_ID in ('"+POI1+"','"+POI2+"')"
+			 query = "SELECT * FROM POIS WHERE Name in ('"+POI1+"','"+POI2+"')"
 	
 		DButilsAzure.execQuery(query)   
 		.then(function(ans2) {
@@ -132,7 +134,7 @@ router.get('/getTwoMostPopularPointsBasedOnUserCategories',function (req,res) {
 
     router.get('/getAllSavedPoints',function (req,res) {
 		let sql ="select POI_ID, NAME, IMAGE, NOV, DESCRIPTION, RANK,"+
-		 		 "CATEGORY from POIS join FAVOURITE_POINTS on  favouritePointID = POI_ID where UserName ='"+currentUserName+"'";
+		 		 "CATEGORY from POIS join FAVOURITE_POINTS on  FAVOURITE_POINTS.POI_ID = POIS.POI_ID where UserName ='"+currentUserName+"'";
 		DButilsAzure.execQuery(sql)   
         .then(function(ans) {
                 res.json({
@@ -146,10 +148,19 @@ router.get('/getTwoMostPopularPointsBasedOnUserCategories',function (req,res) {
 
 
 	 router.post('/saveFavoritePointsToServer',function (req,res) {
-		var POIS_Array =req.body.POIS_Array;
+        var POIS_Array =req.body.POIS_Array;
+        let sql2 = "select top 1 POSITION from FAVOURITE_POINTS UserName ='"+currentUserName+"' order by POSITION desc";
+        DButilsAzure.execQuery(sql2)   
+		.then(function(ans) {
+            if (ans.length != 0){
+                POSITION = ans[0].POSITION;
+            }
+        }
+       )
 		for(var i=0; i<POIS_Array.length; i++){
+            POSITION = POSITION + 1;
 			var POI_ID = POI_Array[i].POI_ID;
-			var sql = "INSERT INTO FAVOURITE_POINTS VALUES ('"+currentUserName+"','"+POI_ID+"')";
+            let sql ="insert into FAVOURITE_POINTS (USER_NAME,POI_ID,POSITION) values('"+currentUserName+"','"+POI_ID+"','"+POSITION+"')";
 			DButilsAzure.execQuery(sql) 
 			.then(function(result){
 				res.sendStatus(200);
@@ -162,8 +173,18 @@ router.get('/getTwoMostPopularPointsBasedOnUserCategories',function (req,res) {
 
 	 
 	 router.post('/saveFavoritePoint', function (req, res) {
-		var POI_ID = req.body.POI_ID;
-		let sql ="insert into FAVOURITE_POINTS values('"+currentUserName+"','"+POI_ID+"')";
+        var POI_ID = req.body.POI_ID;
+        var POSITION = 0;
+        let sql2 = "select top 1 POSITION from FAVOURITE_POINTS UserName ='"+currentUserName+"' order by POSITION desc";
+        DButilsAzure.execQuery(sql2)   
+		.then(function(ans) {
+            if (ans.length != 0){
+                POSITION = ans[0].POSITION + 1;
+            }
+        }
+	   )
+	   .catch(ans=>res.send("FALSE"));
+		let sql ="insert into FAVOURITE_POINTS (USER_NAME,POI_ID,POSITION) values('"+currentUserName+"','"+POI_ID+"','"+POSITION+"')";
 		DButilsAzure.execQuery(sql)   
 		.then(function(ans) {
 			console.log(ans)}
@@ -181,19 +202,18 @@ router.get('/getTwoMostPopularPointsBasedOnUserCategories',function (req,res) {
 		DButilsAzure.execQuery(sql);
 		let sql4 ="UPDATE POIS SET RANK = (RANK * (NOR-1) + '"+rank+"')/NOR  WHERE POI_ID ='"+POI_ID+"'";
 		DButilsAzure.execQuery(sql4);
-		let sql2="select count * as numberOfReviews FROM REVIEWS"; 
-		DButilsAzure.execQuery(sql2)    
-		.then(function(ans) {
-			let Review_Number = ans[0].numberOfReviews;
-			let sql3 ="insert into REVIEWS values('"+Review_Number+"','"+POI_ID+"','"+Review+"',GETDATE())";
+		//let sql2="select count * as numberOfReviews FROM REVIEWS"; 
+		//DButilsAzure.execQuery(sql2)    
+		//.then(function(ans) {
+			let sql3 ="insert into REVIEWS (POI_ID,REVIEW,) values('"+POI_ID+"','"+Review+"',GETDATE())";
 			DButilsAzure.execQuery(sql3)    
 			.then(function(ans) {
 				console.log(ans)
 			}).then(ans=> res.send("TRUE"))
 			.catch(ans=>res.send("FALSE"));
-		})
-		.then(ans=> res.send("TRUE"))
-		.catch(ans=>res.send("FALSE"));
+		//})
+		//.then(ans=> res.send("TRUE"))
+		//.catch(ans=>res.send("FALSE"));
 	 });
 
 
@@ -245,7 +265,7 @@ router.get('/getTwoMostPopularPointsBasedOnUserCategories',function (req,res) {
 //                     // get the decoded payload and header
 //                     var decoded = jwt.decode(token, {complete: true});
 //                     req.decoded= decoded;
-// 					_userName=decoded.payload.userName;					
+// 					currentUserName=decoded.payload.userName;					
 //                     next();
 //                 }
 //             });
@@ -263,38 +283,13 @@ router.get('/getTwoMostPopularPointsBasedOnUserCategories',function (req,res) {
     
 //     })
 
-
-	// router.post('/POIRank',function (req,res) {
-	// 	var POI_Name =req.body.POI_Name;
-	// 	var Rank =req.body.POI_Rank;
-	// 	let sql="INSERT INTO POI_Rank (POI_Name , Rank) values(\'"+POI_Name + "\', \'" +Rank + "\');"; 
-	// 	DButilsAzure.execQuery(sql)    
-	// 	.then(function() {
-	// 		let sql = "select (SUM(Rank)/count(*)) as Aev from POI_Rank where POI_Name ='"+POI_Name+"' " 
-	// 		DButilsAzure.execQuery(sql)    
-	// 		.then(function(ans) {
-	// 			let POI_Aev_Rank = ans[0].Aev
-	// 			let Percentage =  (POI_Aev_Rank/5)*100;
-	// 			let sql = "UPDATE POI SET Rank_Per = "+Percentage+" where POI_Name ='"+POI_Name+"' "
-	// 			DButilsAzure.execQuery(sql)
-	// 			.then(ans=> res.send("then"))
-	// 			.catch(ans=>res.send("catch"));          
-	// 		})
-	// 	})
-	// 	.then(ans=> res.send("TRUE"))
-	// 	.catch(ans=>res.send("FALSE"));
-	//  });
-
  
-	 //add poi review
- 	router.get('/Category/:Category_Name',function (req,res) {
-		var Category_Name = req.params.Category_Name;
-		let sql ="select * from POI where Category_Name ='"+Category_Name+"'";
+ 	router.get('/Category/:Category',function (req,res) {
+		var Category = req.params.Category;
+		let sql ="select * from POIS where Category ='"+Category+"'";
 		DButilsAzure.execQuery(sql)    
 		.then(function(ans) {
-			if (ans.length == 0)
-				return Promise.reject('Wrong POI Name');
-			else if (!(ans[0].Category_Name === Category_Name)) {
+			if (!(ans[0].Category === Category)) {
 				return Promise.reject('Wrong Answer');}
 			else{  
 				res.json({
@@ -305,23 +300,13 @@ router.get('/getTwoMostPopularPointsBasedOnUserCategories',function (req,res) {
 	   .catch(ans=>res.send("catch"));
 	 });
 
-
-	
-	
-   //Return 2 most popular POI by the user’s favourite categories (one for each category) 
-  
-
-
-
-
- router.post('/SetFavPOIOrderByUser',function (req,res) {
-    var UserName = _userName
+ router.post('/setNewOrderForSavedPoints',function (req,res) {
     var POI_Array =req.body.POI_Array;
     var i=0;
-    for(var i=0; j=POI_Array.length,i<j; i++){
-        var POI_Name = POI_Array[i].POI_Name;
-        var POI_Order =  POI_Array[i].Order;
-        var sql = "UPDATE  User_Fav_POI set POI_Order = "+POI_Order+" where UserName = '"+UserName+"' AND POI_Name = '"+POI_Name+"' "
+    for(var i=0; i < POI_Array.length; i++){
+        var POI_ID = POI_Array[i].POI_ID;
+        var POSITION = POI_Array[i].POSITION;
+        var sql = "UPDATE FAVOURITE_POINTS set POSITION = "+POSITION+" where USER_NAME = '"+currentUserName+"' AND POI_ID = '"+POI_ID+"' "
         DButilsAzure.execQuery(sql) 
         .then(function(result){
             res.sendStatus(200);
