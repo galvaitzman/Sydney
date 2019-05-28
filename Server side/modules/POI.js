45826e0d2c5249d7530c2504b76f16c7974df817
glevinha <glevinha@intel.com>
2019-05-28 18:12:22 +0300

    finish 3.2

--- Server side/modules/POI.js
+++ Server side/modules/POI.js
@@ -7,6 +7,24 @@ var Regex = require("regex");
 var jwt = require('jsonwebtoken');
 var  superSecret = "SUMsumOpen";
 var currentUserName = 'galvai';
+var POSITION = 0;
+
+
+router.use('/', (req, res, next) => {
+    var token = req.body.token || req.query.token || req.header('token');  
+    // no token
+    if (!token) res.status(401).send("Access denied. No token provided.");
+    // verify token
+    try {
+        const decoded = jwt.verify(token, 'superSecret');
+        req.decoded = decoded;
+        currentUserName = decoded.USER_NAME;
+        next(); //move on to the actual function
+    } 
+    catch (exception) {
+        res.status(400).send("Invalid token.");
+    }
+});
 
 
 router.get('/getThreePopularRandomPoints',function (req,res) 
@@ -29,12 +47,13 @@ DButilsAzure.execQuery("select * from POIS where RANK >= "+threshHole+"")
 .catch(ans=>res.send("" +ans));
 });
 
-router.get('/getTwoMostPopularPointsBasedOnUserCategories',function (req,res) {
+
+router.get('/getTwoMostPopularPoints',function (req,res) {
 	var sql ="select NAME,CATEGORY from POIS where Category in( select CATEGORY from FAVOURITE_CATEGORIES where USER_NAME ='"+currentUserName+"') order by RANK DESC";
 	DButilsAzure.execQuery(sql)    
 	.then(function(ans) {
         if (ans.length == 0 ){
-            return Promise.reject('no Point of Interest from the given category');
+            res.send('no Point of Interest from the given category');
         }
 		var POI1 = ans[0].NAME;
 		var category1 = ans[0].CATEGORY;		
@@ -65,60 +84,60 @@ router.get('/getTwoMostPopularPointsBasedOnUserCategories',function (req,res) {
  });
 
 
- router.get('/getTwoLastSavedPoints',function (req,res) {
-    let sql ="select top 2 favouritePointID from FAVOURITE_POINTS where UserName ='"+currentUserName+"' order by FAVOURITE_POINTS.ID DESC";
+router.get('/getTwoLastSavedPoints',function (req,res) {
+    let sql ="select top 2 POI_ID from FAVOURITE_POINTS where USER_NAME ='"+currentUserName+"' order by ID DESC";
     DButilsAzure.execQuery(sql)    
     .then(function(ans) {
         if (ans.length == 0)
-            return Promise.reject('no saved points for this user');
+            res.send('no saved points for this user');
         else if (ans.length == 1){  
-            let POI_ID1 = ans[0].favouritePointID;
-            let sql = "select * from POI where POI_Name in ('"+POI_ID1+"')";
+            let POI_ID1 = ans[0].POI_ID;
+            let sql = "select * from POIS where POI_ID in ('"+POI_ID1+"')";
             DButilsAzure.execQuery(sql)    
-            .then(function(ans) {
-                res.json({
-                    poi: ans
-                });	
+            .then(function(ans2) {
+                res.send(ans2); 
             })
         }
         else{
-            let POI_ID1 = ans[0].favouritePointID;
-            let POI_ID2 = ans[1].favouritePointID;
+            let POI_ID1 = ans[0].POI_ID;
+            let POI_ID2 = ans[1].POI_ID;
             let sql = "select * from POIS where POI_ID in ('"+POI_ID1+"','"+POI_ID2+"')";
             DButilsAzure.execQuery(sql)    
-            .then(function(ans) {
-                res.json({
-                    poi: ans
-                });	
+            .then(function(ans3) {
+                res.send(ans3); 
             })
         }
     })
     
-        .catch(ans=>res.send("FALSE"));          
+        .catch(ans=>res.send("," + ans.length + ","));          
     });
 
 
-    router.get('/getTwoLastReviewsOnPoint/:POI_ID',function (req,res) {
-			var POI_ID=req.params.POI_ID;
-			let sql ="UPDATE POIS SET NOV = NOV + 1 WHERE POI_ID ='"+POI_ID+"'";
-            DButilsAzure.execQuery(sql);
-            let sql2 ="select top 2 from REVIEWS where POI_ID ='"+POI_ID+"' order by REVIEW_ID DESC";
-            DButilsAzure.execQuery(sql2)    
+router.get('/getTwoLastReviewsOnPoint',function (req,res) {
+            if (!req.query.POI_ID){
+                res.send("no such attribute POI_ID in the given query");
+            }
+			var POI_ID=req.query.POI_ID;
+			let sql ="UPDATE POIS SET NOV = NOV + 1 WHERE POI_ID ="+POI_ID;
+            DButilsAzure.execQuery(sql)
             .then(function(ans) {
-                if (ans.length == 0)
-                    return Promise.reject('no reviews for this POI');
+                var x=1;
+            })
+			.catch(ans=>res.send("illegal update"));
+            let sql2 ="select top 2 * from REVIEWS where POI_ID ='"+POI_ID+"' order by REVIEW_ID DESC";
+            DButilsAzure.execQuery(sql2)    
+            .then(function(ans2) {
+                if (ans2.length == 0)
+                    res.send('no reviews for this POI');
                 else{  
-                    res.json({
-                        poi: ans
-                    });	
+                    res.send(ans2);
                 }
             })
-            .then(ans=> res.send("then"))
-			.catch(ans=>res.send("catch"));
-    });
+			.catch(ans=>res.send("goni3"));
+});
 
 
-    router.get('/GetAllPoints',function (req,res) 
+router.get('/getAllPoints',function (req,res) 
     {
         let sql ="select * from POIS";
         DButilsAzure.execQuery(sql)   
@@ -129,74 +148,91 @@ router.get('/getTwoMostPopularPointsBasedOnUserCategories',function (req,res) {
             }
        )
        
-    });
+});
 
 
-    router.get('/getAllSavedPoints',function (req,res) {
-		let sql ="select POI_ID, NAME, IMAGE, NOV, DESCRIPTION, RANK,"+
-		 		 "CATEGORY from POIS join FAVOURITE_POINTS on  FAVOURITE_POINTS.POI_ID = POIS.POI_ID where UserName ='"+currentUserName+"'";
+router.get('/getAllSavedPoints',function (req,res) {
+		let sql ="select POIS.POI_ID, NAME, IMAGE, NOV, DESCREPTION, RANK,"+
+		 		 "CATEGORY from POIS join FAVOURITE_POINTS on FAVOURITE_POINTS.POI_ID = POIS.POI_ID where FAVOURITE_POINTS.USER_NAME ='"+currentUserName+"'";
 		DButilsAzure.execQuery(sql)   
         .then(function(ans) {
-                res.json({
-                    poi: ans
-                });	
+                res.send(ans);
             }
        )
-		//.then(ans=> res.send("TRUE"))
-		.catch(ans=>res.send("FALSE"));
-	 });
+		.catch(ans=>res.send(""+ans));
+});
 
 
-	 router.post('/saveFavoritePointsToServer',function (req,res) {
+router.post('/saveFavoritePointsToServer',function (req,res) { //position is +1 even when illegal record is inserted
+        if (!req.body.POIS_Array || !req.body){
+            res.send("no POIS_Array attribute");
+        }
         var POIS_Array =req.body.POIS_Array;
-        let sql2 = "select top 1 POSITION from FAVOURITE_POINTS UserName ='"+currentUserName+"' order by POSITION desc";
+        var POI_ID = 0;
+        var sql2 = "select top 1 POSITION from FAVOURITE_POINTS where USER_NAME = '"+currentUserName+"' order by POSITION desc";
         DButilsAzure.execQuery(sql2)   
 		.then(function(ans) {
             if (ans.length != 0){
                 POSITION = ans[0].POSITION;
             }
+            for(var i=0; i<POIS_Array.length; i++){
+                POSITION = POSITION + 1;
+                if (!req.body.POIS_Array[i].POI_ID){
+                    res.send("no POI_ID attribute in the " + i + " index");
+                }
+                POI_ID = POIS_Array[i].POI_ID;
+                var sql ="insert into FAVOURITE_POINTS (USER_NAME,POI_ID,POSITION) values('"+currentUserName+"','"+POI_ID+"',"+POSITION+")";
+                DButilsAzure.execQuery(sql) 
+                .then(function(result){
+                    
+                }).catch(function(err){
+                    res.send(err);
+                })
+            }   
         }
-       )
-		for(var i=0; i<POIS_Array.length; i++){
-            POSITION = POSITION + 1;
-			var POI_ID = POI_Array[i].POI_ID;
-            let sql ="insert into FAVOURITE_POINTS (USER_NAME,POI_ID,POSITION) values('"+currentUserName+"','"+POI_ID+"','"+POSITION+"')";
-			DButilsAzure.execQuery(sql) 
-			.then(function(result){
-				res.sendStatus(200);
-			}).catch(function(err){
-				res.send(err);
-			})
-		}   
+       ).catch(function(err){
+        res.send("," + POI_ID + ",");
+    })
 	
-	 });
-
-	 
-	 router.post('/saveFavoritePoint', function (req, res) {
+	
+});
+     
+     
+router.post('/saveFavoritePoint', function (req, res) {
+        if (!req.body.POI_ID){
+            res.send('no POI_ID attribute');
+        }
         var POI_ID = req.body.POI_ID;
-        var POSITION = 0;
-        let sql2 = "select top 1 POSITION from FAVOURITE_POINTS UserName ='"+currentUserName+"' order by POSITION desc";
+        var POSITION=0;
+        
+        var sql2 = "select top 1 POSITION from FAVOURITE_POINTS where User_Name ='"+currentUserName+"' order by POSITION desc";
         DButilsAzure.execQuery(sql2)   
 		.then(function(ans) {
-            if (ans.length != 0){
+            ans3 = ans;
+            if (ans.length > 0){
+                
                 POSITION = ans[0].POSITION + 1;
             }
+            var sql ="insert into FAVOURITE_POINTS (USER_NAME,POI_ID,POSITION) values ('"+currentUserName+"','"+POI_ID+"',"+POSITION+")";
+		    DButilsAzure.execQuery(sql)   
+		    .then(function(ans) {
+			    console.log(ans)}
+	        )
+	        .then(ans=> res.send("TRUE"))
+	        .catch(ans=>res.send("," +POSITION+ ","));
         }
-	   )
-	   .catch(ans=>res.send("FALSE"));
-		let sql ="insert into FAVOURITE_POINTS (USER_NAME,POI_ID,POSITION) values('"+currentUserName+"','"+POI_ID+"','"+POSITION+"')";
-		DButilsAzure.execQuery(sql)   
-		.then(function(ans) {
-			console.log(ans)}
-	   )
-	   .then(ans=> res.send("TRUE"))
-	   .catch(ans=>res.send("FALSE"));
-	});
+       )
+       .catch(ans=>res.send(ans));
+		
+});
 
 
-	router.post('/reviewPoint',function (req,res) {
+router.post('/reviewPoint',function (req,res) {
+        if (!req.body.POI_ID || !req.body.REVIEW || !req.body.rank){
+            res.send('one of the requierd attribute was not provided');
+        }
 		var POI_ID =req.body.POI_ID;
-		var Review =req.body.Review;
+		var Review =req.body.REVIEW;
 		var rank = req.body.rank;
 		let sql ="UPDATE POIS SET NOR = NOR + 1 WHERE POI_ID ='"+POI_ID+"'";
 		DButilsAzure.execQuery(sql);
@@ -205,35 +241,34 @@ router.get('/getTwoMostPopularPointsBasedOnUserCategories',function (req,res) {
 		//let sql2="select count * as numberOfReviews FROM REVIEWS"; 
 		//DButilsAzure.execQuery(sql2)    
 		//.then(function(ans) {
-			let sql3 ="insert into REVIEWS (POI_ID,REVIEW,) values('"+POI_ID+"','"+Review+"',GETDATE())";
+			let sql3 ="insert into REVIEWS (POI_ID,REVIEW,DATE) values('"+POI_ID+"','"+Review+"',GETDATE())";
 			DButilsAzure.execQuery(sql3)    
 			.then(function(ans) {
 				console.log(ans)
 			}).then(ans=> res.send("TRUE"))
-			.catch(ans=>res.send("FALSE"));
+			.catch(function(err){
+                res.send(err);
+            })
 		//})
 		//.then(ans=> res.send("TRUE"))
 		//.catch(ans=>res.send("FALSE"));
-	 });
+});
 
 
-	 router.delete('/removeFavoritePoint', function (req, res, next) {
+router.delete('/removeFavoritePoint', function (req, res) {
+        if (!req.body.POI_ID){
+            res.send("");
+        }
 		var POI_ID=req.body.POI_ID;
-        var sql ="DELETE FROM  FAVOURITE_POINTS WHERE POI_ID='"+POI_ID+"' AND UserName = '"+currentUserName+"'";  
+        var sql ="DELETE FROM  FAVOURITE_POINTS WHERE POI_ID='"+POI_ID+"' AND USER_NAME = '"+currentUserName+"'";  
         DButilsAzure.execQuery(sql)
         .then( res.send("TRUE"))
         .catch(ans=>res.send("FALSE"));
-	});
+});
 
 
 
-
-
-
-
-
-
- function shuffle(array) {
+function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
   // While there remain elements to shuffle...
@@ -252,58 +287,58 @@ router.get('/getTwoMostPopularPointsBasedOnUserCategories',function (req,res) {
   return array;
 }
 
-// router.use('/', function (req, res, next) {
-//         // check header or url parameters or post parameters for token
-//         var token = req.body.token || req.query.token || req.headers['token'];  
-//         // decode token
-//         if (token) {
-//             jwt.verify(token, 'superSecret', function (err, decoded) {
-//                 if (err) {
-//                     return res.json({ success: false, message: 'Failed to authenticate token.' });
-//                 } else {
-//                     // if everything is good, save to request for use in other routes
-//                     // get the decoded payload and header
-//                     var decoded = jwt.decode(token, {complete: true});
-//                     req.decoded= decoded;
-// 					currentUserName=decoded.payload.userName;					
-//                     next();
-//                 }
-//             });
-    
-//         } 
-// 	else {
-    
-//             // if there is no token
-//             // return an error
-//             return res.status(403).send({
-//                 success: false,
-//                 message: 'No token provided.'
-//             });
-//         }
-    
-//     })
+// router.use("/", (req, res) => {
+//     const token = req.header("x-auth-token");
+//     // no token
+//     if (!token) res.status(401).send("Access denied. No token provided.");
+//     // verify token
+//     try {
+//         const decoded = jwt.verify(token, secret);
+//         req.decoded = decoded;
+//         if (req.decoded.admin)
+//             res.status(200).send({ result: "Hello admin." });
+//         else
+//             res.status(200).send({ result: "Hello user." });
+//     } catch (exception) {
+//         res.status(400).send("Invalid token.");
+//     }
+// });
+
+
+
 
  
- 	router.get('/Category/:Category',function (req,res) {
-		var Category = req.params.Category;
-		let sql ="select * from POIS where Category ='"+Category+"'";
+router.get('/Category',function (req,res) {
+        if (!req.query.CATEGORY){
+            res.send('no CATEGORY attribute');
+        }
+		var Category = req.query.CATEGORY;
+		let sql ="select * from POIS where CATEGORY ='"+Category+"'";
 		DButilsAzure.execQuery(sql)    
 		.then(function(ans) {
-			if (!(ans[0].Category === Category)) {
-				return Promise.reject('Wrong Answer');}
+            if (ans.length ==0){
+                res.send(ans);
+            }
+			else if (!(ans[0].CATEGORY=== Category)) {
+				res.send(Category);}
 			else{  
-				res.json({
-					poi: ans
-				});	
+				res.send(ans);
 			}
 		})
 	   .catch(ans=>res.send("catch"));
-	 });
+});
 
- router.post('/setNewOrderForSavedPoints',function (req,res) {
+
+router.post('/setNewOrderForSavedPoints',function (req,res) {
+    if (!req.body.POI_Array){
+        res.send('no POI_Array attribute');
+    }
     var POI_Array =req.body.POI_Array;
     var i=0;
     for(var i=0; i < POI_Array.length; i++){
+        if (!POI_Array[i].POI_ID || POI_Array[i].POSITION){
+
+        }
         var POI_ID = POI_Array[i].POI_ID;
         var POSITION = POI_Array[i].POSITION;
         var sql = "UPDATE FAVOURITE_POINTS set POSITION = "+POSITION+" where USER_NAME = '"+currentUserName+"' AND POI_ID = '"+POI_ID+"' "
@@ -314,7 +349,7 @@ router.get('/getTwoMostPopularPointsBasedOnUserCategories',function (req,res) {
             res.send(err);
         })
     }   
- });
+});
 
 
 module.exports = router;
--- Server side/modules/USERS.js
+++ Server side/modules/USERS.js
@@ -6,35 +6,222 @@ var DButilsAzure = require('../DButils');
 //var Regex = require("regex");
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var  superSecret = "SUMsumOpen";
+var Countries = ["Ausralia","Bolovia","China","Denemark","Israel","Latvia","Monaco","August","Norway","Panama","Switzerland","USA"];
+
+
 
 router.post("/Login", function(req, res) {
     var userPass = req.body;
     var name = userPass.USER_NAME;
-    var pass = userPass.PASSWORD;
+    var password = userPass.PASSWORD;
     DButilsAzure.execQuery("SELECT PASSWORD FROM USERS WHERE USER_NAME= '"+name+"'")
-      .then(function(result) {
+    .then(function(result) {
         if(result.length == 0)
             return Promise.reject('Wrong Username');
         else if (!(result[0].PASSWORD === userPass.PASSWORD)) {
-            return Promise.reject('Wrong Password');}      
-            /*
-            NEED TO ADD CODE TO SEND TOKEN
-            */
-            //sendToken(userName,password,res)	
-        else return Promise.resolve("log in succesfully");	
-      })
-      .then(function(result){
-        console.log(result);
-        res.status(200).send(result);
-      })
-      .catch(function(err) {
-        console.log(err);
-        res.status(400).send(err);
-      });
-  });
+            return Promise.reject('Wrong Password');
+        }      
+        else{
+            payload = {  USER_NAME: name, admin: true };
+            options = { expiresIn: "1d" };
+            const token = jwt.sign(payload, 'superSecret', options);
+            res.send(token);
+        }   
+           		        
+      }
+    )
+    .catch(ans=>res.send("FALSE"));
+});
+
+router.post('/Register', function (req, res) {
+  
+    if(!req.body){
+        res.send("Register Failure");
+        return; 
+    }
+    var userName=req.body.USER_NAME;
+    if (userName.length>8 || userName.length<3 ){
+        res.send("User name should contain 3 to 8 letters");
+        return;
+    }
+    if (req.body.PASSWORD.length>10 || req.body.PASSWORD.length<5 ){
+        res.send("password should contain 5 to 10 letters/digits");
+        return;
+    }
+    if(!/^[a-zA-Z]+$/.test(userName)){
+        res.send("User name should contain only letters");
+        return;
+    }
+    var lettersNumbers  =/^[0-9a-zA-Z]+$/;
+    
+    if(!(lettersNumbers.test(req.body.PASSWORD))){
+        res.send("password should contain only letters and numbers");
+        return;
+    }
+    DButilsAzure.execQuery("select * from USERS where USER_NAME= '"+userName+"'")
+    .then(function(ans){   
+        if(ans.length != 0){ 
+            res.send("Please Choose Another User Name");  
+            return;    
+        }    
+    });
+
+    if (!req.body.QuestionsAnswers || req.body.QuestionsAnswers.length<2){
+        res.send("not enough answers provided. at least 2 answer requierd");
+        return;
+    }  
+
+    if (!req.body.favoriteCategories || req.body.favoriteCategories.length<2){
+        res.send("not enough categories provided. at least 2 categories requierd");
+        return;
+    } 
+    
+    if (!req.body.Country || !Countries.includes(req.body.Country)){
+        res.send(req.body.Country);
+        return;
+    }
+
+    var wrongCategory = 0;
+    for (var i=0; i< req.body.favoriteCategories.length; i=i+1) {
+        let sql3 =  "select * from CATEGORIES where CATEGORY='"+ req.body.favoriteCategories[i] + "'";
+        DButilsAzure.execQuery(sql3)
+        .then(function(ans){
+            if (ans.length==0){
+                res.send("one or more of the categories does not exist. please provide correct categories");
+                res.end();
+                process.exit();
+                return;
+            }
+        });        
+    }
+
+    // if (wrongCategory > 0){
+    //     res.send("one or more of the categories does not exist. please provide correct categories");
+    //     return;
+    // }
+
+    
+    var firstName=req.body.FirstName;
+    var lastName=req.body.LastName;
+    var city=req.body.City;
+    var country = req.body.Country;
+    var email=req.body.Email;
+    var password=req.body.PASSWORD; 
+    
+
+    let sql="insert into Users values(\'"+userName + "\', \'" +password + "\', \'" + firstName +
+    "\', \'" + lastName + "\',\'" + city + "\',\'" + country + "\', \'" + email + "\');";
+    DButilsAzure.execQuery(sql)
+    .then(function(ans){
+       
+        for (var i=0; i< req.body.QuestionsAnswers.length; i=i+1) {
+            let sql2 =  "insert into ANSWERS_USER values('" + userName + "','" + req.body.QuestionsAnswers[i].QUESTION_ID + "','" + req.body.QuestionsAnswers[i].ANSWER + "')";
+            DButilsAzure.execQuery(sql2);
+        }
+         
+        for (var i=0; i< req.body.favoriteCategories.length; i=i+1) {
+            let sql4 =  "insert into FAVOURITE_CATEGORIES values('" + userName + "','" + req.body.favoriteCategories[i] + "')";
+            DButilsAzure.execQuery(sql4)
+            .then(function(ans){
+                res.send("registed successfuly");
+            })        
+            .catch(ans=>res.send("something went wrong. please try again"));
+        }
+        
+        //sendToken(userName,password,res) ;
+      
+    })        
+    .catch(ans=>res.send("error" +ans));
+});
+
+
+
+router.get('/getAllQuestions',function(req,res) {
+    let sql = "select * from QUESTIONS";
+    DButilsAzure.execQuery(sql)
+    .then(function(ans){
+        res.send(ans);
+    })        
+    .catch(ans=>res.send("error"));
+});
+
+
+router.get('/getRandomQuestion',function(req,res) {
+    if (!req.query.USER_NAME){
+        res.send("no USER_NAME attribute");
+        return;
+    }
+    var userName = req.query.USER_NAME;
+    let sql = "select QUESTION_ID from ANSWERS_USER where USER_NAME = '" + userName +"'";
+    DButilsAzure.execQuery(sql)
+    .then(function(ans){
+        if (ans.length != 0){
+            var rand = ans[Math.floor(Math.random() * ans.length)];
+            res.send(rand);
+        }
+    })        
+    .catch(ans=>res.send("error"));
+});
+
+
+router.post('/RetrievePassword',function (req,res) {
+    var userToCheck=req.body;
+    if(!userToCheck){
+        res.send("Restore Password Failure");
+        res.end();
+        return;    
+    }
+    var userName=userToCheck.USER_NAME;
+    var answer = userToCheck.ANSWER;
+    var question = userToCheck.QUESTION;
+    DButilsAzure.execQuery("select * from ANSWERS_USER where USER_NAME = '"+userName+"' and QUESTION_ID ='"+question+"'")    
+        .then(function(ans) {		   
+            if (ans.length == 0)
+                return Promise.reject('the given user did not answer the given question when registerd');		
+            else
+            {
+                if (ans[0].ANSWER == answer){
+                    DButilsAzure.execQuery("select PASSWORD from USERS where USER_NAME = '"+userName+"'")
+                    .then(function(ans2) {
+                        res.send(ans2[0].PASSWORD);		   
+                    })
+                    .catch(ans2=>res.send("error"));
+                }
+                else{
+                    res.send('wrong answer. please try again');
+                }
+            }
+        })
+    .catch(ans=>res.send("" +ans));
+});
+
+
+// router.post('/authenticate', function (req, res) {
+
+// if (!req.body.userName || !req.body.password)
+//     res.send({ message: "bad values" })
+
+// else {
+
+//     for (id in Users) {
+//         var user = Users[id]
+
+//         if (req.body.userName == user.userName)
+//             if (req.body.password == user.password)
+//                 sendToken(user, res)
+//             else {
+//                 res.send({ success: false, message: 'Authentication failed. Wrong Password' })
+//                 return
+//             }
+//     }
+
+//     res.send({ success: false, message: 'Authentication failed. No such user name' })
+// }
+
+// })
 
 
 
 
 
-  module.exports = router;
+module.exports = router;
