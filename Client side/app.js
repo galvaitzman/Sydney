e19038bfd2792a730fadaeee989b3f074493f3ee
Inbaroth <inbaroth1993@gmail.com>
2019-06-15 15:39:50 +0300

    hello + username

--- Client side/app.js
+++ Client side/app.js
@@ -1,48 +1,47 @@
-let app = angular.module('myApp', ["ngRoute"]);
+let app = angular.module("myApp", ["ngRoute"]);
 
 // config routes
-app.config(function($routeProvider)  {
-    $routeProvider
-        // homepage
-        .when('/', {
-            // this is a template
-            templateUrl: 'pages/home/home.html',
-            controller : 'HomeController as homeCtrl'
-        })
-        // about
-        .when('/about', {
-            // this is a template url
-            templateUrl: 'pages/about/about.html',
-            controller : 'aboutController as abtCtrl'
-        })
-        .when('/login', {
-            templateUrl: 'pages/login/login.html',
-            controller : 'loginController as loginCtrl'
-        })
-        .when('/register', {
-            templateUrl: 'pages/register/register.html',
-            controller : 'registerController as registerCtrl',
-        })
-        .when('/forgotPassword', {
-            // this is a template url
-            templateUrl: 'pages/forgotPassword/forgotPassword.html',
-            controller : 'forgotPasswordController as forgotPassCtrl'
-        })
-        .when('/POI', {
-            // this is a template url
-            templateUrl: 'pages/POI/POI.html',
-            controller : 'POIController as POICtrl'
-        })
+app.config(function($routeProvider) {
+  $routeProvider
+    // homepage
+    .when("/", {
+      // this is a template
+      templateUrl: "pages/home/home.html",
+      controller: "HomeController as homeCtrl"
+    })
+    // about
+    .when("/about", {
+      // this is a template url
+      templateUrl: "pages/about/about.html",
+      controller: "aboutController as abtCtrl"
+    })
+    .when("/login", {
+      templateUrl: "pages/login/login.html",
+      controller: "loginController as loginCtrl"
+    })
+    .when("/register", {
+      templateUrl: "pages/register/register.html",
+      controller: "registerController as registerCtrl"
+    })
+    .when("/forgotPassword", {
+      // this is a template url
+      templateUrl: "pages/forgotPassword/forgotPassword.html",
+      controller: "forgotPasswordController as forgotPassCtrl"
+    })
+    .when("/POI", {
+      // this is a template url
+      templateUrl: "pages/POI/POI.html",
+      controller: "POIController as POICtrl"
+    })
 
-        .when('/favorite', {
-            // this is a template url
-            templateUrl: 'pages/favorite/favorite.html',
-            controller : 'favoriteController as favoriteCtrl'
-        })
+    .when("/favorite", {
+      // this is a template url
+      templateUrl: "pages/favorite/favorite.html",
+      controller: "favoriteController as favoriteCtrl"
+    })
 
+    // other
+    .otherwise({ redirectTo: "/" });
 
-        // other
-        .otherwise({ redirectTo: '/' });
-
-    // TODO: to fix in the html Hello User
+  // TODO: to fix in the html Hello User
 });
--- Client side/pages/home/homeController.js
+++ Client side/pages/home/homeController.js
@@ -1,30 +1,41 @@
-
-angular.module("myApp").service('HomeServices',[ '$http', function ($http) {
-
-    this.random3POI = function () {
+angular
+  .module("myApp")
+  .service("HomeServices", [
+    "$http",
+    function($http) {
+      this.random3POI = function() {
         var req = {
-            method: 'GET',
-            url: 'http://localhost:3000/POI/getThreePopularRandomPoints',
-            headers: {
-                'Access-Control-Allow-Origin' : '*',
-                'Access-Control-Allow-Methods' :"GET, POST, PUT, DELETE, OPTIONS",
-                'Access-Control-Allow-Headers' : '*',
-                'Access-Control-Max-Age' : '*',
-                'Content-Type': 'application/json'
-            }
+          method: "GET",
+          url: "http://localhost:3000/POI/getThreePopularRandomPoints",
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Max-Age": "*",
+            "Content-Type": "application/json"
+          }
         };
         return $http(req);
-
+      };
     }
+  ])
 
-}])
-
-    .controller('HomeController', function ($scope,$http,HomeServices){
-
-        HomeServices.random3POI().then(function (response) {
-            $scope.random3POIlist = response.data;
-        }, function (response) {
-            self.Login.content = "Get 3 random POI failed";
-        });
-
-    });
+  .controller("HomeController", function(
+    $scope,
+    $http,
+    HomeServices,
+    $rootScope
+  ) {
+    $scope.current_user = "Guest";
+    if ($rootScope.currentUser != null && $rootScope.currentUser != "") {
+      $scope.current_user = $rootScope.currentUser;
+    } //DONE by inbar
+    HomeServices.random3POI().then(
+      function(response) {
+        $scope.random3POIlist = response.data;
+      },
+      function(response) {
+        self.Login.content = "Get 3 random POI failed";
+      }
+    );
+  });
